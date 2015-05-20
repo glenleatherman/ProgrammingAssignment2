a2abe7f4ef9179d4b92226760f0567dd4ee8e727
Glen Leatherman <gleatherman@ILSWKS-110.ILS.LOCAL>
2015-05-20 15:00:55 -0500

    tested version
    seems to be working.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix versions of makeVector and cachemean
+## Glen Leatherman
 
-## Write a short comment describing this function
+## create a package of get,set functions for a matrix and its inversion (solve).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## either solve and cache or retrieve from cache the inversion of a matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+      message("getting cached data")    
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
